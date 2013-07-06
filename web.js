4b09fa34baf45070ae9f10f9d0976560be647abf
imughal <mughalscomputers@gmail.com>
2013-07-06 18:06:34 +0500

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var data = fs.readFileSync("./index.html");
+var buf = new Buffer(data);
+var index = buf.toString("utf-8", 0, 130)
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+//  console.log(index);
 });
