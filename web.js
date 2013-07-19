87d3d94c9118c5d04c86a1eb6431f69d2d53af2a
imughal <mughalscomputers@gmail.com>
2013-07-19 20:59:24 +0500

    updated web.js

--- web.js
+++ web.js
@@ -11,5 +11,4 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-//  console.log(index);
 });
